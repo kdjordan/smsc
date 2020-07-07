8600ba7d7eae1081f7c592ad6d5f343955479062
Kevin Jordan <k.dean.jordan@gmail.com>
2020-07-07 12:57:57 -0700

    feat: routes adding routes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
     <div id="nav">
+    <TopNav />
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import TopNav from '@/components/TopNav'
+
+export default {
+  components: TopNav
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/TopNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+      <p>Some Text</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+.header {
+    background: red;
+}
+</style>
